3b2f2b252dc9b74ede2bf9ddccc2808d06bdecad
Leo79Jp <leojparodi@gmail.com>
2024-08-04 16:51:48 -0300

    complete los ejercicios 9 y 10

--- ejercicio10.js
+++ ejercicio10.js
@@ -1,2 +1,14 @@
 
     // Ejercicio 10: Utilizar un bucle for...of para iterar sobre un array de autos e imprimir sus descripciones.
+        modelos = [
+            {marca: "Ford", modelo: "Ka", año:2000},
+            {marca: "Fiat", modelo: "Palio", año: 2015},
+            {marca: "Renault", modelo: "clio", año: 2019},
+            {marca: "Chevrolet", modelo: "Corsa", año: 2103},
+            {marca: "Ford", modelo: "Ranger", año: 2024}
+        ]
+
+        for (const autos of modelos) {
+           console.log(autos);
+        };
+
